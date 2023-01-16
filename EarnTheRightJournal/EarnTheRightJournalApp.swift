d9fdc9d584d6aecd8bb6b9f8cdf9ce21c41a878e
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-01-16 13:56:49 -0700

    Initial Commit

---
+++ EarnTheRightJournal/EarnTheRightJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarnTheRightJournalApp.swift
+//  EarnTheRightJournal
+//
+//  Created by Auston Youngblood on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EarnTheRightJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
